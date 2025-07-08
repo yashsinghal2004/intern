be73c23620b1e96eed1e67936c9e3439869e4823
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2025-07-08 15:42:01 +0530

    Complete pagination for 150 products

--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,22 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
+
 const ProductCard=({image,title,price})=>{
   return(
-    <div className="flex border p-2 w-sm flex-wrap flex-col m-2">
-      <Image src={image} alt={title} width={200} height={200} />
+    <div className="flex border p-2 w-50 h-auto flex-wrap flex-col m-1">
+      <Image className="flex items-center justify-center" src={image} alt={title} width={100} height={100} />
       <h2>{title}</h2>
-      <p>Price: Rs. {price}</p>
+      <p className="pt-3">Price: Rs. {price}</p>
     </div>
   )
 }
 
+const PAGE_SIZE=8;
+
 export default function Home() {
   const[products,setProducts]=useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
 
   const fetchData=async ()=>{
     const data=await fetch("https://fakestoreapi.in/api/products?limit=150")
@@ -24,11 +28,45 @@ export default function Home() {
     fetchData();
   },[]);
 
+  const tot_products=products.length;
+  const noofpages=Math.ceil(tot_products/PAGE_SIZE);
+
+  const start=currentPage* PAGE_SIZE;
+  const end=start+PAGE_SIZE;
+
+  const handlePageChange=(n)=>{
+    setCurrentPage(n);
+  }
+
+  const handlePrevPage=()=>{
+    setCurrentPage((prev)=>prev-1);
+  }
+
+  const handleNextPage=()=>{
+    setCurrentPage((prev)=>prev+1);
+  }
+
   return !products.length ?<h1>No Products</h1> : (
     <div >
+      <h1 className="justify-center items-center flex text-2xl p-2">Product-Catalog</h1>
+      <div className="justify-center items-center flex p-1 ">
+        
+        {[...Array(noofpages).keys()].map((n)=>(<button 
+        className={`p-2 m-0.5 border cursor-pointer hover:bg-amber-200 ${n === currentPage ? "bg-amber-200" : ""}`}
+
+         key={n} 
+          onClick={()=>handlePageChange(n)}>
+          {n}
+          </button>))}
+          
+      </div>
+      <div className="justify-center items-center flex">
+      <button hidden={currentPage===0} className="p-1 m-0.5 border cursor-pointer" onClick={()=>handlePrevPage()} >◀ </button>
+      <button hidden={currentPage===noofpages-1} className="p-1 m-0.5 border cursor-pointer" onClick={()=>handleNextPage()}>▶</button>
+          </div>   
       <div className="flex flex-wrap">
       {
-        products.map((prod)=>(
+        products.slice(start,end).map((prod)=>(
           <ProductCard key={prod.id} title={prod.title} image={prod.image} price={prod.price}/>
         ))
       }
